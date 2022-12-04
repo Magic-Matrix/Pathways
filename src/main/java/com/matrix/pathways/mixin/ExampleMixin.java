f70cc8543b1bd848b60d1dd23b8255ad65b3b1e6
Magic-Matrix <w_jiashuai@163.com>
2022-12-04 14:17:58 +0800

    first commit

---
+++ src/main/java/com/matrix/pathways/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package com.matrix.pathways.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.matrix.pathways.Pathways;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Pathways.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
